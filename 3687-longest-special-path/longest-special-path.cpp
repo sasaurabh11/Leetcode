e2da60b436d53ba4a5631e3643495c6bdadccc62
Saurabh <160242066+sasaurabh11@users.noreply.github.com>
2025-01-22 01:31:17 +0530

    Time: 179 ms (81.00%) | Memory: 316.8 MB (17.89%) - LeetSync

---
+++ 3687-longest-special-path/longest-special-path.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> nums;
+    int ans = 0, count = 1;
+    vector<int> depth;
+
+    void dfs(int node, int par, vector<vector<pair<int, int>>>& graph, int left, int curr_level, vector<int>& pre) {
+        int prev_level = depth[nums[node]];
+        depth[nums[node]] = curr_level;
+
+        left = max(left, prev_level);
+        if(pre.back() - pre[left] >= ans) {
+            if(pre.back() - pre[left] == ans) count = min(count, curr_level - left);
+            else {
+                ans = pre.back() - pre[left];
+                count = curr_level - left;
+            }
+        }
+
+        for(auto [nbr, wt] : graph[node]) {
+            if(nbr != par) {
+                pre.push_back(pre.back() + wt);
+                dfs(nbr, node, graph, left, curr_level + 1, pre);
+                pre.pop_back();
+            }
+        }
+
+        depth[nums[node]] = prev_level;
+    }
+
+    vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
+        int n = nums.size();
+        this->nums = nums;
+        vector<vector<pair<int, int>>> graph(n);
+
+        for (auto& val : edges) {
+            graph[val[0]].emplace_back(val[1], val[2]);
+            graph[val[1]].emplace_back(val[0], val[2]);
+        }
+
+        vector<int> pre = {0};
+        depth = vector<int>(50100, 0);
+        dfs(0, -1, graph, 0, 1, pre);
+
+        return {ans, count};
+    }
+};
