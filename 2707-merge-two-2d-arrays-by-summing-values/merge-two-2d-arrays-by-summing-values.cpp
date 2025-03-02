16d973023b4222359ac7643856cbead90a64a52b
Saurabh <160242066+sasaurabh11@users.noreply.github.com>
2025-03-02 23:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (44.73%) - LeetSync

---
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        int i = 0, j = 0, n = nums1.size(), m = nums2.size();
+
+        vector<vector<int>> ans;
+        while(i < n && j < m) {
+            if(nums1[i][0] <= nums2[j][0]) {
+                if(!ans.empty() && ans[ans.size() - 1][0] == nums1[i][0]) 
+                    ans[ans.size() - 1][1] += nums1[i][1];
+                else ans.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                if(!ans.empty() && ans[ans.size() - 1][0] == nums2[j][0]) 
+                    ans[ans.size() - 1][1] += nums2[j][1];
+                else ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        while(i < n) {
+            if(!ans.empty() && ans[ans.size() - 1][0] == nums1[i][0]) 
+                ans[ans.size() - 1][1] += nums1[i][1];
+            else ans.push_back(nums1[i]);
+            i++;
+        }
+
+        while(j < m) {
+            if(!ans.empty() && ans[ans.size() - 1][0] == nums2[j][0]) 
+                ans[ans.size() - 1][1] += nums2[j][1];
+            else ans.push_back(nums2[j]);
+            j++;
+        }
+
+        return ans;
+    }
+};
