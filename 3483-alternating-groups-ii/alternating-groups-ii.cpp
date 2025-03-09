a33bfd5d2756efca407555dca6d4f4c495772eb8
Saurabh <160242066+sasaurabh11@users.noreply.github.com>
2025-03-09 23:58:06 +0530

    Time: 76 ms (37.24%) | Memory: 111.1 MB (16.74%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -5,18 +5,14 @@ public:
         for(int i = 0; i < n; i++) 
             colors.push_back(colors[i]);
 
-        vector<int> pre;
-        for(int i = 1; i < n + k; i++) {
-            if(colors[i] == colors[i - 1]) pre.push_back(1);
+        int count = 1, ans = 0;
+        for(int i = 1; i < n + k - 1; i++) {
+            if(colors[i] == colors[i - 1]) count = 1;
             else {
-                if(pre.empty()) pre.push_back(1);
-                else pre.push_back(pre.back() + 1);
+                count++;
+                if(count >= k) ans++;
             }
         }
-
-        int ans = 0;
-        for(auto val : pre) 
-            if(val >= k) ans++;
 
         return ans;
     }
