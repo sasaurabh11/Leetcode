9d11a77f94eed1bf1232326c4a33e54396ccd0a9
Saurabh <160242066+sasaurabh11@users.noreply.github.com>
2025-03-12 00:16:24 +0530

    Time: 11 ms (51.39%) | Memory: 11.1 MB (57.30%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -4,16 +4,14 @@ public:
         int i = 0, j = 0, n = s.length();
         
         int ans = 0;
-        map<char, int> st;
+        vector<int> st(3, 0);
         for(i = 0; i < n; i++) {
-            st[s[i]]++;
-            while(j <= i && st['a'] >= 1 && st['b'] >= 1 && st['c'] >= 1) {
+            st[s[i] - 'a']++;
+            while(j <= i && st[0] >= 1 && st[1] >= 1 && st[2] >= 1) {
                 ans += n - i;
-                st[s[j]]--;
+                st[s[j] - 'a']--;
                 j++;
             }
-
-            cout << i << " " << ans << endl;
         }
 
         return ans;
